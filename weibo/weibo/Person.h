f1c4948fc4b1d1edb890f6f1ab7e4556d8498696
yst@163.com <yangshutong@yangshutongdemacbook-pro.local>
2016-11-04 14:53:46 +0800

    增加Person类

---
+++ weibo/weibo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weibo
+//
+//  Created by 杨书通 on 16/11/4.
+//  Copyright © 2016年 杨书通. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weibo/weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weibo
+//
+//  Created by 杨书通 on 16/11/4.
+//  Copyright © 2016年 杨书通. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
